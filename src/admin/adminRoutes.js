76473c2400afd97582712b8ad1de7b4335a82bb8
Sembarang28 <agilfikriawan2803@gmail.com>
2024-08-06 18:05:06 +0800

    add: permission feature

--- src/admin/adminRoutes.js
+++ src/admin/adminRoutes.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const userController = require("./user/userController");
 const adminSession = require("./middleware/adminSession");
 const adminProfileController = require("./profile/profileController");
+const adminPermissionController = require("./permission/permissionController");
 
 const adminRoutes = new Router();
 
 adminRoutes.use("/user", adminSession, userController);
 adminRoutes.use("/profile", adminSession, adminProfileController);
+adminRoutes.use("/permission", adminSession, adminPermissionController);
 
 module.exports = adminRoutes;
---
+++ src/admin/permission/permissionController.js
@@ -0,0 +1,118 @@
+const { Router } = require("express");
+const multer = require("../../config/multer");
+const sharp = require("../../config/sharp");
+const permissionModel = require("./permissionModel");
+const response = require("../../config/response");
+
+const adminPermissionController = new Router();
+
+adminPermissionController.post(
+  "/",
+  multer.permissionImg("image"),
+  async (req, res) => {
+    const schema = Joi.object({
+      userId: Joi.string().required(),
+      permission: Joi.string().required(),
+      information: Joi.optional(),
+      photo: Joi.optional(),
+    });
+
+    const validation = schema.validate(req.body);
+
+    if (validation.error) {
+      const errorDetails = validation.error.details.map(
+        (detail) => detail.message,
+      );
+
+      const responseBody = {
+        status: false,
+        message: "failed",
+        code: 422,
+        error: errorDetails.join(", "),
+      };
+
+      return response(res, responseBody);
+    }
+
+    if (req.file) {
+      sharp.permissionImg(req.file);
+    }
+    const imageName = sharp.filename;
+    const imagePath = imageName ? `public/permissionImg/${imageName}` : "";
+
+    const createPermission = await permissionModel.createPermission(
+      req.body,
+      imagePath,
+    );
+    sharp.filename = "";
+    return response(res, createPermission);
+  },
+);
+
+adminPermissionController.get("/all", async (req, res) => {
+  const { search, page } = req.query;
+  const readAllPermission = await permissionModel.readAllPermission(
+    page,
+    search,
+  );
+  return response(res, readAllPermission);
+});
+
+adminPermissionController.get("/:permissionId", async (req, res) => {
+  const { permissionId } = req.params;
+  const readPermissionById = await permissionModel.readPermissionById(
+    Number(permissionId),
+  );
+  return response(res, readPermissionById);
+});
+
+adminPermissionController.put("/:permissionId", async (req, res) => {
+  const { permissionId } = req.params;
+  const schema = Joi.object({
+    userId: Joi.string().required(),
+    permission: Joi.string().required(),
+    information: Joi.optional(),
+    photo: Joi.optional(),
+    verify: Joi.string().required(),
+  });
+
+  const validation = schema.validate(req.body);
+
+  if (validation.error) {
+    const errorDetails = validation.error.details.map(
+      (detail) => detail.message,
+    );
+
+    const responseBody = {
+      status: false,
+      message: "failed",
+      code: 422,
+      error: errorDetails.join(", "),
+    };
+
+    return response(res, responseBody);
+  }
+
+  if (req.file) {
+    sharp.permissionImg(req.file);
+  }
+  const imageName = sharp.filename;
+  const imagePath = imageName ? `public/permissionImg/${imageName}` : "";
+
+  const updatePermissionById = await permissionModel.updatePermissionById(
+    Number(permissionId),
+    req.body,
+    imagePath,
+  );
+  return response(res, updatePermissionById);
+});
+
+adminPermissionController.delete("/:permissionId", async (req, res) => {
+  const { permissionId } = req.params;
+  const deletePermissionById = await permissionModel.deletePermissionById(
+    Number(permissionId),
+  );
+  return response(res, deletePermissionById);
+});
+
+module.exports = adminPermissionController;
